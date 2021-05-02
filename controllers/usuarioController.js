c0d2c5de0351310d1769e34d1c267757a3b30a5c
Luis21 <huamanluis808@gmail.com>
2021-05-02 16:36:09 -0500

    Se realiza la craeacion para el authcontroller

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,6 +1,7 @@
 const Usuario = require("../models/Usuario");
 const bcryptjs = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
 
 exports.crearUsuario = async (req, res) => {
   //Revisar si hay errores
@@ -28,8 +29,25 @@ exports.crearUsuario = async (req, res) => {
     //guardar usuario
     await usuario.save();
 
-    //mensaje de confirmacion
-    res.json({ msg: "El usuario creado correctamente" });
+    //crear y firmar el jwt
+    const payload = {
+      usuario: {
+        id: usuario.id,
+      },
+    };
+    //Firmar el jwt
+    jwt.sign(
+      payload,
+      process.env.SECRETA,
+      {
+        expiresIn: 3600,
+      },
+      (error, token) => {
+        if (error) throw error;
+        //mensaje de confirmacion
+        res.json({ token });
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(400).send("Hubo un error");
---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+//Rutas para authenticar usuario
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/authController");
+const { check } = require("express-validator");
+
+//crear un usuario
+
+//api/usuario
+router.post("/",
+  [
+    check("email", "Agrega un email valido").isEmail(),
+    check("password", "El password debe ser minimo de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  authController.autenticarUsuario
+);
+module.exports = router;
