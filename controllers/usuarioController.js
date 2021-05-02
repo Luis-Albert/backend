7a5d55ca267d8cc9f820d8ff8ec0d2e419ddc469
Luis21 <huamanluis808@gmail.com>
2021-05-02 16:19:46 -0500

    Se realiza el hasheo para los password, se agrega el express validator, recordar que se deben instalar los plugins de hash y exxpress validator

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,17 +1,35 @@
 const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 exports.crearUsuario = async (req, res) => {
-  try {
-    let usuario;
+  //Revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
 
+  //extraer email y password
+  const { email, password } = req.body;
+  try {
+    //Validar que el usuario registrado sea unico
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({ msg: "El usuario ya existe" });
+    }
     //guardar el nuevo usuario
     usuario = new Usuario(req.body);
+
+    //Hashear el password
+    const salt = await bcryptjs.genSaltSync(10);
+    usuario.password = await bcryptjs.hash(password, salt);
 
     //guardar usuario
     await usuario.save();
 
     //mensaje de confirmacion
-    res.send("Usuario creado correctamente");
+    res.json({ msg: "El usuario creado correctamente" });
   } catch (error) {
     console.log(error);
     res.status(400).send("Hubo un error");
