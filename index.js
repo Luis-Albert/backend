84e3c9155d317c8a8a849e2418b98865bc5ffbe8
Luis21 <huamanluis808@gmail.com>
2021-05-02 15:13:30 -0500

    Se configura la conexion a DB desde mongoose hacia monogAtlas

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const express = require('express')
+const express = require("express");
+const conectarDB = require("./config/db");
+
+//Crear el servidor
+const app = express();
+
+//conectar a la base de datos
+conectarDB();
+
+//Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//Definir la pagina principal
+app.get("/", (req, res) => {
+  res.send("Hello Putito");
+});
+
+//arrancar la app
+app.listen(PORT, () => {
+  console.log(`El servidor esta funcionado en el puerto ${PORT}`);
+});
