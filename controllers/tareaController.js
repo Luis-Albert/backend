ac9bda86e6cbf7f7697349cf7578fc5b1a7bb6f7
Luis21 <huamanluis808@gmail.com>
2021-05-03 23:19:47 -0500

    Se cambiar el tipo de recepcion de datos para obener los proyectos, de req.body a req.query

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -37,7 +37,7 @@ exports.crearTarea = async (req, res) => {
 exports.obtenerTareas = async (req, res) => {
   try {
     //extraer el proyecto y comprobar si existe
-    const { proyecto } = req.body;
+    const { proyecto } = req.query;
     const existeProyecto = await Proyecto.findById(proyecto);
     if (!existeProyecto) {
       return res.status(404).json({ msg: "Proyecto no encontrado" });
